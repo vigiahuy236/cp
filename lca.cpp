021c16a25725b32f1926c1bdba80f9589cf7b6cc
vigiahuy236 <vigiahuy2009@gmail.com>
2025-12-17 15:51:07 +0700

    add lca

---
+++ lca.cpp
@@ -0,0 +1,61 @@
+/**
+ *    author:  Vi Gia Huy
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 3e5 + 4;
+int n, root;
+int par[N], high[N];
+vector<int> adj[N];
+
+void dfs1(int u) {
+    for (int v : adj[u]) {
+        if (v == par[u]) continue;
+        par[v] = u;
+        high[v] = high[u] + 1;
+        dfs1(v);
+    }
+}
+
+void setup(int root) {
+    par[root] = root;
+    high[root] = 0;
+    dfs1(root);
+    return;
+}
+
+int lca(int u, int v) {
+    if (high[u] < high[v]) swap(v, u);
+    while (high[u] > high[v]) u = par[u];
+    if (u == v) return u;
+    while (u != v) {
+        u = par[u];
+        v = par[v]; 
+    }
+    return u;
+}
+
+void sol() {
+    cin >> n >> root;
+    for (int i = 1; i < n; i++) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    setup(root);
+    cout << lca(5, 8);
+    return;
+} 
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    freopen("lca.inp", "r", stdin);
+    freopen("lca.out", "w", stdout);
+    int T = 1;
+    //cin >> T;
+    while (T--) sol();
+    return 0;
+}
