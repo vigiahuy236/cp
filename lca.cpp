c8b985c6ce76704eb4cac518f60bdb12fcf61b77
vigiahuy236 <vigiahuy2009@gmail.com>
2026-02-05 21:52:23 +0700

    update

--- lca.cpp
+++ lca.cpp
@@ -110,8 +110,8 @@ void sol() {
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    freopen("lca.inp", "r", stdin);
-    freopen("lca.out", "w", stdout);
+    // freopen("lca.inp", "r", stdin);
+    // freopen("lca.out", "w", stdout);
     int T = 1;
     //cin >> T;
     while (T--) sol();
